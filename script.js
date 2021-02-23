3fa6324b378805677595c1368f5cee0644a8945c
Catalina Bestard <cati@iMac-de-Catalina.local>
2021-02-22 20:23:28 -0500

    script2

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-// window.onload = choosePic;
-
-// var imgArray = [
-//     'images/home/Bestard-Fragments-1.jpg',
-//     'images/home/Bestard-Fragments-2.jpg',
-//     'images/home/Bestard-Fragments-3.jpg',
-//     'images/home/Bestard-Fragments-4.jpg',
-//     'images/home/Bestard-Fragments-5.jpg',
-//     'images/home/Bestard-Fragments-6.jpg',
-//     'images/home/Bestard-Fragments-7.jpg',
-//     'images/home/Bestard-Fragments-8.jpg',
-//     'images/home/Bestard-Fragments-9.jpg',
-//     'images/home/Bestard-Fragments-10.jpg',
-//     'images/home/Bestard-Fragments-11.jpg',
-//     'images/home/Bestard-Fragments-12.jpg',
-//     'images/home/Bestard-Fragments-13.jpg',
-//     'images/home/Bestard-Fragments-14.jpg',
-//     'images/home/Bestard-Fragments-15.jpg',
-//     'images/home/Bestard-Fragments-16.jpg',
-//     ];
-
-// function choosePic() {
-//     var randomNum = Math.floor(Math.random() * imgArray.length);
-//     document.getElementById("myPicture").src = imgArray[randomNum];
-//     console.log ("functioning");
-// };
 
 window.addEventListener('DOMContentLoaded', choosePic);
 function choosePic(event) {
@@ -43,7 +17,7 @@ function choosePic(event) {
     // start with 1
     for(let i=1; i <= 11; i++){
         // 14 is missing!!!
-        if(i === 10) continue;
+        if(i === 11) continue;
         images.push(`images/home/Bestard-Fragments-${i}.jpg`);
     }
     const index = Math.floor(Math.random() * images.length);
